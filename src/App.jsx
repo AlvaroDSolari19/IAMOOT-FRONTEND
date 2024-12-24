f7014784c65edca660a30b39d93be473a70d6425
AlvaroDSolari19 <AlvaroDSolari19@outlook.com>
2024-12-24 16:54:22 -0500

    Added Routers to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,16 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 import Button from 'react-bootstrap/Button'; 
 
-import './App.css'
+/*********
+ * PAGES *
+ *********/
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage'; 
+import CompSelectPage from './pages/CompSelectPage';
+import OralCompPage from './pages/OralCompPage'
+import WrittenCompPage from './pages/WrittenCompPage'
+
+//import './App.css'
 
 function App() {
 
@@ -15,7 +24,18 @@ function App() {
     </div>
   )*/
 
-    
+    return (
+        <Router>
+            <Routes>
+                <Route path='/' element={<HomePage/>} />
+                <Route path='/login' element={<LoginPage/>} />
+                <Route path='/compselect' element={<CompSelectPage/>} />
+                <Route path='/oralcomp' element={<OralCompPage/>}/>
+                <Route path='/writtencomp' element={<WrittenCompPage/>} />
+            </Routes>
+        </Router>
+    ); 
+
 }
 
 export default App
