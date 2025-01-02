8161abe8cbf62de41d2c25b04c01abda5b18320d
AlvaroDSolari19 <AlvaroDSolari19@outlook.com>
2025-01-02 03:04:11 -0500

    Added Multiple Languages to the Login Page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,12 +1,48 @@
-import React from 'react'; 
-import NavigateButton from '../components/NavigateButton';
+import React, { useContext } from 'react'; 
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom'; 
+import { Button, Form} from 'react-bootstrap';
+
+import LanguageContext from '../contexts/LanguageContext';
 
 const LoginPage = () => {
+
+    const { currentLanguage } = useContext(LanguageContext); 
+    const performNavigation = useNavigate(); 
+    const { register, handleSubmit, formState: { errors }, } = useForm();
+    
+    /****************************************************
+     * REVIEW LATER ONCE WE HAVE LOGGING IN INFORMATION *
+     ****************************************************/
+    const onSubmit = (someData) => {
+        console.log(someData);
+        performNavigation('/compselect')
+    };
+
+    const formLabels = {
+        EN: {mainTitle: 'Access the Platform', theUsername: 'Username', thePassword: 'Password', theButton: 'Sign In'},
+        ES: {mainTitle: 'Acceso a la Plataforma', theUsername: 'Usuario', thePassword: 'Contraseña', theButton: 'Iniciar Sesion'}, 
+        POR: {mainTitle: 'Acesse a Plataforma', theUsername: 'Usuário', thePassword: 'Senha', theButton: 'Entrar'}
+    };
+
+    const actualLabels = formLabels[currentLanguage]
+
     return <div>
-        <h1>Access Dashboard</h1>
-        <p>Contains a form with email and password and a button below it to submit.</p>
-        <p>Because a form acts different than regular buttons for navigation, I will have to do more research on how to set it up.</p>
-        <NavigateButton to='/compselect'>Proceed</NavigateButton>
+        <h1>{actualLabels.mainTitle}</h1>
+
+        <Form onSubmit={handleSubmit(onSubmit)}>
+            <Form.Group className='mb-3'>
+                <Form.Label>{actualLabels.theUsername}</Form.Label>
+                <Form.Control type='email' placeholder='Enter email' {...register('username', {required: true})} />
+            </Form.Group>
+
+            <Form.Group className='mb-3'>
+                <Form.Label>{actualLabels.thePassword}</Form.Label>
+                <Form.Control type='password' placeholder='Password' {...register('password', {required: true})} />
+            </Form.Group>
+
+            <Button variant='primary' type='submit'>{actualLabels.theButton}</Button>
+        </Form>
     </div>
 };
 
